f55ac3c028d0fdc6e60deaa30b62bc5f8d7c8c4f
c00lestg1rl <vasquez.samantha697@student.ccm.edu>
2026-09-25 01:17:21 -0400

    Completed Operations

---
+++ arithmetic_operations.py
@@ -0,0 +1,32 @@
+# Samantha Vasquez 
+# CMP131: Fundamentals of Programming
+# Week 4, Lab 2
+# September 24,2026
+
+# Program Title 
+print('****Program for Basic Arithmetic Operations***')
+
+# User Input 
+value1= float(input('Enter a number:'))
+value2= float(input('Enter a second number that is not 0:'))
+
+# Calculations
+addition_result= value1 + value2
+
+subtraction_result= value1 - value2
+
+multiplication_result= value1 * value2
+
+division_result= value1 / value2 
+
+power_result= (value1) ** value2
+
+average= ((addition_result) / 2)
+
+# Output
+print('Addition Operation Result:', addition_result)
+print('Subtraction Operation Result:', subtraction_result)
+print('Multiplication Operation result:', multiplication_result)
+print('Division Operation Result:', division_result)
+print('Exponential and Power Operation Result:', power_result)
+print('Operation result for finding the average:', average)
